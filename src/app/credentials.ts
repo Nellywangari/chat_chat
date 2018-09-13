4cbb031167aed0237cc4f673aa7cdc0433612c12
Nelly <nellywangarih.gmail.com>
2018-09-13 17:35:12 +0300

    first commit

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firabaseConfig= {
+    apiKey: "AIzaSyAIPg2uyjHXg564P-tBo1SZ5J47AbtZj4E",
+    authDomain: "trial-3db3d.firebaseapp.com",
+    databaseURL: "https://trial-3db3d.firebaseio.com",
+    projectId: "trial-3db3d",
+    storageBucket: "trial-3db3d.appspot.com",
+    messagingSenderId: "924519742047",
+}
